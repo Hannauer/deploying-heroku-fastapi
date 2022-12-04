b33971b9feaf508554114b0384b0979ba18090e4
Hannauer <joaopedrohannauer@gmail.com>
2022-12-04 16:31:03 -0300

    fixing api tests

--- unit_tests/test_api.py
+++ unit_tests/test_api.py
@@ -14,10 +14,10 @@ def test_get_method():
 
     client_response = client.get("/")
     assert client_response.status_code == 200
-    assert client_response.son() == {"message": "Greetings!"}
+    assert client_response.json() == {"greeting": "Welcome!"}
 
 
-def test_post_high(client):
+def test_post_high():
     request = client.post("/infer/", json={"age": 31,
                                     "workclass": "Private",
                                     "fnlgt": 209642,
@@ -31,4 +31,39 @@ def test_post_high(client):
                                     "capital_gain": 14084,
                                     "native_country": "United-States"})
     assert request.status_code == 200
-    assert request.json() == {"prediction": ">50K"}
+    assert request.json() == {"preds": ">50K"}
+
+
+def test_post_low():
+    request = client.post("/infer/", json={
+                                            "age": 39,
+                                            "workclass": "State-gov",
+                                            "fnlgt": 77516,
+                                            "education": "Bachelors",
+                                            "education_num": 13,
+                                            "marital_status": "Never-married",
+                                            "occupation": "Exec-managerial",
+                                            "relationship": "Not-in-family",
+                                            "race": "White",
+                                            "sex": "Male",
+                                            "capital_gain": 2174,
+                                            "native_country": "United-States"
+                                            })
+    assert request.status_code == 200
+    assert request.json() == {"preds": "<=50K"}
+
+def test_wrong_format():
+    request = client.post("/infer/", json={
+                                            "age": 39,
+                                            "workclass": "State-gov",
+                                            "fnlgt": 77516,
+                                            "education": "",
+                                            "education_num": 13,
+                                            "marital_status": "",
+                                            "occupation": "Exec-managerial",
+                                            "relationship": "Not-in-family",
+                                            "race": "White",
+                                            "sex": "",
+                                            "native_country": "United-States"
+                                            })
+    assert request.status_code == 422
