3d7ae25064a1a976f9bd11356539715fd7b50caf
Hannauer <joaopedrohannauer@gmail.com>
2022-12-04 19:37:30 -0300

    trying to fix heroku

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starter.load_and_infer import start_inference
 
-# if "DYNO" in os.environ and os.path.isdir(".dvc"):
-#     os.system("dvc config core.no_scm true")
-#     if os.system("dvc pull") != 0:
-#         exit("dvc pull failed")
-#     os.system("rm -r .dvc .apt/usr/lib/dvc")
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 class InputData(BaseModel):
