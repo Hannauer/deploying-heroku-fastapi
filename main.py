97b6ec479680c2d79bc555e4afe1165eee60d329
Hannauer <joaopedrohannauer@gmail.com>
2022-12-04 19:04:51 -0300

    change main

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
+
 class InputData(BaseModel):
     age: int
     workclass: str
