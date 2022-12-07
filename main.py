356d34e783349b411db62db88851a4def35a9b36
Hannauer <joaopedrohannauer@gmail.com>
2022-12-07 19:23:03 -0300

    updating screenshots

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starter.load_and_infer import start_inference
 
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+# if "DYNO" in os.environ and os.path.isdir(".dvc"):
+#     os.system("dvc config core.no_scm true")
+#     if os.system("dvc pull") != 0:
+#         exit("dvc pull failed")
+#     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 
 class InputData(BaseModel):
-    age: int
-    workclass: str
-    fnlgt: int
-    education: str
-    education_num: int
-    marital_status: str
-    occupation: str
-    relationship: str
-    race: str
-    sex: str
-    capital_gain: int
-    native_country: str
+    age: int = 21
+    workclass: str = "State-gov"
+    fnlgt: int = 77516
+    education: str = "Bachelors"
+    education_num: int = 13
+    marital_status: str = "Never-married"
+    occupation: str = "Exec-managerial"
+    relationship: str = "Not-in-family"
+    race: str ="White"
+    sex: str = "Male",
+    capital_gain: int = 2174
+    native_country: str = "United-States"
 
 # Instantiate the app.
 app = FastAPI()
