7445be6000d2cc2de34d897ad419caec47641a3b
Hannauer <joaopedrohannauer@gmail.com>
2022-12-07 20:19:33 -0300

    fix::base model examples

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from starter.load_and_infer import start_inference
 
 # if "DYNO" in os.environ and os.path.isdir(".dvc"):
@@ -14,17 +14,17 @@ from starter.load_and_infer import start_inference
 
 class InputData(BaseModel):
     age: int = 21
-    workclass: str = "State-gov"
-    fnlgt: int = 77516
-    education: str = "Bachelors"
-    education_num: int = 13
-    marital_status: str = "Never-married"
-    occupation: str = "Exec-managerial"
-    relationship: str = "Not-in-family"
-    race: str ="White"
-    sex: str = "Male",
-    capital_gain: int = 2174
-    native_country: str = "United-States"
+    workclass: str = Field(..., example="State-gov")
+    fnlgt: int = Field(..., example=77516)
+    education: str = Field(..., example="Bachelors")
+    education_num: int = Field(..., example=13)
+    marital_status: str = Field(..., example="Never-married")
+    occupation: str = Field(..., example="Exec-managerial")
+    relationship: str = Field(..., example="Not-in-family")
+    race: str =Field(..., example="White")
+    sex: str = Field(..., example="Male")
+    capital_gain: int = Field(..., example=2174)
+    native_country: str = Field(..., example="United-States")
 
 # Instantiate the app.
 app = FastAPI()
